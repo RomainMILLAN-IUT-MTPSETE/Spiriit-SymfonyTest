f7e0d13bfe15c8eb8556e4cb3c230133171bd992
MILLAN Romain <hello@romainmillan.fr>
2022-12-16 10:53:40 +0100

    Product List

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController{
 
     #[Route('/produits', name: 'product_list')]
-    public function list(): Response{
-        return $this->render('products/list.html.twig');
+    public function list(ManagerRegistry $doctrine): Response{
+        $entityManager = $doctrine->getManager();
+
+        $products = $entityManager->getRepository(Product::class)->findAll();
+
+
+        return $this->render('products/list.html.twig', ['products' => $products]);
     }
 
 }
